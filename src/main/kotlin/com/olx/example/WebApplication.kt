ae34b7691395f5c421a115e5494c61bfb514c838
Maryna Cherniavska <maryna.cherniavska@olx.com>
2020-05-25 21:24:33 +0200

    fixed an error in logging

--- src/main/kotlin/com/olx/example/WebApplication.kt
+++ src/main/kotlin/com/olx/example/WebApplication.kt
@@ -7,11 +7,16 @@ import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
 
 fun main() {
     embeddedServer(Netty, 8080) {
+        logger.debug { "Embedded Netty server created..." }
         routing {
             get("/") {
+                logger.debug("GET /")
                 call.respondText("<h1>My Example Application</h1>", ContentType.Text.Html)
             }
         }
